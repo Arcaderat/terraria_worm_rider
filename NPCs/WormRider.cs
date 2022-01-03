51e3e7eda95c33f8425c97c5451cf4335edc7fb4
Parker <pkswanson1@outlook.com>
2022-01-03 18:48:40 -0500

    Empty classes for mod and NPC

---
+++ NPCs/WormRider.cs
@@ -0,0 +1,9 @@
+using Terraria.ModLoader;
+
+namespace WormRiderBoss.NPCs
+{
+	public class WormRider : ModNPC
+	{
+        
+	}
+}
--- WormRiderBoss.cs
+++ WormRiderBoss.cs
@@ -4,5 +4,17 @@ namespace WormRiderBoss
 {
 	public class WormRiderBoss : Mod
 	{
+		//We shouldn't need to add anything to this, it should just work automatically from the inherited class
+		public WormRiderBoss(){
+			// By default, all Autoload properties are True. You only need to change this if you know what you are doing.
+			//Properties = new ModProperties()
+			//{
+			//	Autoload = true,
+			//	AutoloadGores = true,
+			//	AutoloadSounds = true,
+			//	AutoloadBackgrounds = true
+			//};
+		}
+
 	}
 }
