57d5d084452cdb95a74c3fbedcd72c14549ec30c
Parker <pkswanson1@outlook.com>
2022-01-03 20:41:55 -0500

    Spawner and basic NPC for WormRider

---
+++ Items/WormRiderSpawner.cs
@@ -0,0 +1,43 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace WormRiderBoss.Items
+{
+	public class WormRiderSpawner : ModItem
+	{
+        public override bool UseItem(Player player) {
+			NPC.SpawnOnPlayer(player.whoAmI, ModContent.NPCType<NPCs.WormRider>());
+			Main.PlaySound(SoundID.Roar, player.position, 0);
+			return true;
+		}
+
+        public override void AddRecipes(){
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(9);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+
+        }
+        
+        public override void SetDefaults() {
+			item.width = 20;
+			item.height = 20;
+			item.maxStack = 20;
+			item.rare = ItemRarityID.Cyan;
+			item.useAnimation = 45;
+			item.useTime = 45;
+			item.useStyle = ItemUseStyleID.HoldingUp;
+			item.UseSound = SoundID.Item44;
+			item.consumable = true;
+		}
+        
+        // We use the CanUseItem hook to prevent a player from using this item while the boss is present in the world.
+		public override bool CanUseItem(Player player) {
+            //TODO only allow spawning in desert?
+            //TODO only allow one spawn
+			//prevent the player from spawning more than one at a time
+			return true;
+		}
+	}
+}
--- NPCs/WormRider.cs
+++ NPCs/WormRider.cs
@@ -4,6 +4,22 @@ namespace WormRiderBoss.NPCs
 {
 	public class WormRider : ModNPC
 	{
-        
+		
+		public override void SetDefaults() {
+			npc.aiStyle = 2;
+			npc.lifeMax = 40000;
+			npc.damage = 100;
+			npc.defense = 55;
+			npc.knockBackResist = 0f;
+			npc.width = 100;
+			npc.height = 100;
+			npc.boss = true;
+			npc.noGravity = true;
+			npc.noTileCollide = true;
+		}
+		//Sets the random natural spawn rate to 0
+        public override float SpawnChance(NPCSpawnInfo spawnInfo){
+			return 0.0F;
+		}
 	}
 }
